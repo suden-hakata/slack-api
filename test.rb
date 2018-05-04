aaf35f30bc79e62946edf97673e83bae7a736a4e
Hiroki Shiota <yuki041137784@gmail.com>
2018-05-04 17:34:15 +0900

    Add testslackbot

--- test.rb
+++ test.rb
@@ -2,5 +2,15 @@ require 'http'
 require 'json'
 require "pry"
 
-response = HTTP.post("https://slack.com/api/auth.test")
+# ユーザー名を入力
+USERNAME = ""
+
+response = HTTP.post("https://slack.com/api/chat.postMessage", params: {
+    token: ENV['SLACK_API_TOKEN'],
+    channel: USERNAME,
+    text: "test",
+    as_user: false,
+    username: "slack_bot"
+  })
+
 puts JSON.pretty_generate(JSON.parse(response.body))
